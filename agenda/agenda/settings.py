a33a738dcec8125879d0282d33db6cfdfa6aeecb
AlexVictorB <alexvictorbarreto@hotmail.com>
2021-09-29 17:50:16 -0300

    Add App Core

--- agenda/agenda/settings.py
+++ agenda/agenda/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r^c3=^%k8e@l=h$+dmt8$9xmx=5(@fysw4n*!jzd)(e^7ub#wz'
+SECRET_KEY = 'django-insecure-!1x(z-^*t2fujx@w0%5yy8v7=ki-ldm+0di%+ztyh4mzoo1r08'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField()
+    data_criacao = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'evento'
