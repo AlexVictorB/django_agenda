f9eeb804c764e888e120df056d18869786006b42
AlexVictorB <alexvictorbarreto@hotmail.com>
2021-10-04 17:14:00 -0300

    Tabela de eventos no BD para trabalhar com agenda

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from core.models import Evento
 # Register your models here.
 
-admin.site.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+    #list_filter = ('usuario', 'data_evento',)
+
+admin.site.register(Evento, EventoAdmin)
